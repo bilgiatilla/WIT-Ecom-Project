39acab00e28ce540ffc0371ea0a14bdc7acc9288
bilgiatilla <atillabilgi.ab@gmail.com>
2026-03-27 11:13:23 +0300

    add-slider-components
    using swiper

--- src/Pages/HomepageSections/MainSlider.jsx
+++ src/Pages/HomepageSections/MainSlider.jsx
@@ -1,11 +1,29 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
 function MainSlider() {
   return (
     <section>
+       <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        spaceBetween={0}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        loop={true}
+        className="hero-swiper h-220"
+      >
+        <SwiperSlide>
       <div className="h-160 lg:h-220 bg-cover bg-center text-white" 
       style={{ backgroundImage: "url('/src/assets/hero-slide.jpg')"}}>
-        <div className="grid text-center lg:text-start pt-40 lg:pt-50 mx-18 lg:mx-30 gap-10">
+        <div className="grid text-center lg:text-start pt-40 lg:pt-60 mx-18 lg:mx-30 gap-10">
         <h5 className="font-bold">SUMMER 2020</h5>
-        <h2 className="font-bold text-3xl lg:text-5xl">NEW COLLECTION</h2>
+        <h2 className="font-bold text-3xl lg:text-6xl">NEW COLLECTION</h2>
         <h4 className="text-[#FAFAFA] lg:text-xl">
           We know how large objects will act, <br></br>but things on a small scale.
         </h4>
@@ -14,6 +32,23 @@ function MainSlider() {
         </div>
         </div>
       </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="h-160 lg:h-220 bg-cover bg-center text-white" 
+      style={{ backgroundImage: "url('/src/assets/hero-slide.jpg')"}}>
+        <div className="grid text-center lg:text-start pt-40 lg:pt-60 mx-18 lg:mx-30 gap-10">
+        <h5 className="font-bold">SUMMER 2020</h5>
+        <h2 className="font-bold text-3xl lg:text-6xl">NEW COLLECTION</h2>
+        <h4 className="text-[#FAFAFA] lg:text-xl">
+          We know how large objects will act, <br></br>but things on a small scale.
+        </h4>
+        <div className="font-bold">
+        <button className="border bg-[#2DC071] border-[#2DC071] px-5 py-2">SHOP NOW</button>
+        </div>
+        </div>
+      </div>
+      </SwiperSlide>
+      </Swiper>
     </section>
   );
 }
--- src/Pages/HomepageSections/VitaClassic.jsx
+++ src/Pages/HomepageSections/VitaClassic.jsx
@@ -1,7 +1,24 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 function VitaClassic() {
   return (
+    <section>
+       <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        spaceBetween={0}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        loop={true}
+        className="hero-swiper"
+      >
+        <SwiperSlide>
      <div className="lg:flex lg:flex-row lg:h-150 lg:w-full">
     <section className="bg-[#23856D] h-100 lg:h-full pt-40 lg:w-1/2">
     <div className="text-center lg:text-left text-[#FFFFFF] px-15 lg:px-40 grid gap-5">
@@ -18,6 +35,27 @@ function VitaClassic() {
     <img src="src/assets/vita-cover.png" alt="cover" className="lg:w-auto lg:h-full lg:object-contain pt-20" />
   </section>
   </div>
+  </SwiperSlide>
+  <SwiperSlide>
+    <div className="lg:flex lg:flex-row lg:h-150 lg:w-full">
+    <section className="bg-[#23856D] h-100 lg:h-full pt-40 lg:w-1/2">
+    <div className="text-center lg:text-left text-[#FFFFFF] px-15 lg:px-40 grid gap-5">
+    <h4 className="lg:text-2xl">SUMMER 2020</h4>
+    <h2 className="text-3xl lg:text-6xl font-bold">Vita Classic Product</h2>
+    <h4 className="lg:text-2xl">We know how large objects will act, but things on a small scale.</h4>
+    <div className="lg:flex lg:flex-row lg:gap-10">
+    <h3 className="font-bold text-2xl lg:text-3xl">$16.48</h3>
+    <button className="bg-[#2DC071] font-bold px-5 py-2 text-sm rounded-sm">ADD TO CART</button>
+    </div>
+    </div>
+  </section>
+  <section className="bg-[#23856D] lg:w-1/2 flex justify-center">
+    <img src="src/assets/vita-cover.png" alt="cover" className="lg:w-auto lg:h-full lg:object-contain pt-20" />
+  </section>
+  </div>
+  </SwiperSlide>
+  </Swiper>
+  </section>
   );
 }
 
