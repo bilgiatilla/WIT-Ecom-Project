ca3048ddf5eb5e20be1529903e2b09d0f8cbd478
bilgiatilla <atillabilgi.ab@gmail.com>
2026-04-05 04:09:39 +0300

    Update Bestseller.jsx

--- src/Components/Bestseller.jsx
+++ src/Components/Bestseller.jsx
@@ -1,22 +1,23 @@
 import { Link } from "react-router-dom";
+import bestseller1 from "../assets/Bestseller/bestseller1.png";
 
 function Bestseller() {
   const products = [
-    { id: 1, image: "src/assets/Bestseller/bestseller1.png" },
-    { id: 2, image: "src/assets/Bestseller/bestseller1.png" },
-    { id: 3, image: "src/assets/Bestseller/bestseller1.png" },
-    { id: 4, image: "src/assets/Bestseller/bestseller1.png" },
-    { id: 5, image: "src/assets/Bestseller/bestseller1.png" },
-    { id: 6, image: "src/assets/Bestseller/bestseller1.png" },
-    { id: 7, image: "src/assets/Bestseller/bestseller1.png" },
-    { id: 8, image: "src/assets/Bestseller/bestseller1.png" },
+    { id: 1, image: bestseller1 },
+    { id: 2, image: bestseller1 },
+    { id: 3, image: bestseller1 },
+    { id: 4, image: bestseller1 },
+    { id: 5, image: bestseller1 },
+    { id: 6, image: bestseller1 },
+    { id: 7, image: bestseller1 },
+    { id: 8, image: bestseller1 },
   ];
   return (
     <section className="px-10 lg:px-40 justify-center">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
       {products.map((product) => (
         <Link key={product.id} to={`/product/${product.id}`}>
-        <div key={product.id} className="text-center grid gap-2 py-5 justify-center">
+        <div className="text-center grid gap-2 py-5 justify-center">
           <img src={product.image} />
 
           <h3 className="font-bold">Graphic Design</h3>
